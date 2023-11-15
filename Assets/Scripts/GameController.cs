4be37564ea23d300d547cdcbfc67dcf2e62802b4
Vahé Krikorian <vahekrikorian@MacBook-Pro-2.mshome.net>
2023-11-16 00:56:46 +0100

    implement correctly the menu in the game

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum GameState { FreeRoam, Dialogue }
+public enum GameState { FreeRoam, Dialogue, Menu }
 
 public class GameController : MonoBehaviour
 {
@@ -12,23 +12,33 @@ public class GameController : MonoBehaviour
 
     GameState state;
 
-    /*private void Awake() {
-        ConditionsDB.Init();
-    }*/
+    MenuController menuController;
 
-    /*private void Start() {
+    private void Awake() {
+        menuController = GetComponent<MenuController>();
+        // ConditionsDB.Init();
+    }
+
+    private void Start() {
         //player.OnEncountered += StartBattle;
         //battleSystem.OnBattleOver += EndBattle;
 
-        DialogueManager.Instance.OnShowDialogue += () => {
-            state = GameState.Dialogue;
+        // DialogueManager.Instance.OnShowDialogue += () => {
+        //     state = GameState.Dialogue;
+        // };
+
+        // DialogueManager.Instance.OnCloseDialogue += () => {
+        //     if (state == GameState.Dialogue)
+        //         state = GameState.FreeRoam;
+        // };
+
+        menuController.onBack += () =>
+        {
+            state = GameState.FreeRoam;
         };
 
-        DialogueManager.Instance.OnCloseDialogue += () => {
-            if (state == GameState.Dialogue)
-                state = GameState.FreeRoam;
-        };
-    }*/
+        menuController.onMenuSelected += OnMenuSelected;
+     }
 
     /*void StartBattle() {
         state = GameState.Battle;
@@ -47,15 +57,48 @@ public class GameController : MonoBehaviour
         worldCamera.gameObject.SetActive(true);
     }*/
 
-    private void Update() {
-        if(state == GameState.FreeRoam) {
+    private void Update()
+    {
+        if (state == GameState.FreeRoam)
+        {
             playerController.HandleUpdate();
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                menuController.OpenMenu();
+                state = GameState.Menu;
+            }
         }
         /*else if (state == GameState.Battle) {
             battleSystem.HandleUpdate()
         }*/
-        else if (state == GameState.Dialogue) {
+        else if (state == GameState.Dialogue)
+        {
             DialogueManager.Instance.HandleUpdate();
         } 
+        else if (state == GameState.Menu)
+        {
+            menuController.HandleUpdate();
+        }
+    }
+
+    void OnMenuSelected(int selectedItem)
+    {
+        if (selectedItem == 0)
+        {
+
+        }
+        else if (selectedItem == 1)
+        {
+            
+        }
+        else if (selectedItem == 2)
+        {
+            
+        }
+        else if (selectedItem == 3)
+        {
+            
+        }
     }
 }
